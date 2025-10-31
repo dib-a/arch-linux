5865e4e30745f13bd7e4097d46ec2e66ea07d9bf
dib-a <dib.ahmad.000@gmail.com>
2025-10-31 23:02:08 +0100

    made encryption optional

--- installation.sh
+++ installation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ================================================================
-#  Arch Linux Minimal Installation Script (with Full LUKS2 Encryption)
-#  Author: [Your Name]
+#  Arch Linux Minimal Installation Script (Encrypted or Plain Root)
+#  Author: dib-a
 #  License: MIT
 # ================================================================
 
@@ -14,15 +14,19 @@ DEFAULT_USERNAME="user"
 DEFAULT_TIMEZONE="Europe/Berlin"
 DEFAULT_LOCALE="en_US.UTF-8"
 DEFAULT_KEYMAP="us"
+DEFAULT_ENCRYPT="y"
 # ================================================================
 
 echo "=== Arch Linux Minimal Installation Script ==="
-echo "This will ERASE the selected disk and install Arch Linux with full LUKS2 encryption."
+echo "This will ERASE the selected disk and install Arch Linux."
 echo
 
 # --- User Inputs with Defaults ---
 read -rp "Enter target disk [${DEFAULT_DISK}]: " DISK
 DISK=${DISK:-$DEFAULT_DISK}
+
+read -rp "Encrypt root partition? [y/N] [${DEFAULT_ENCRYPT}]: " ENCRYPT
+ENCRYPT=${ENCRYPT:-$DEFAULT_ENCRYPT}
 
 read -rp "Enter hostname [${DEFAULT_HOSTNAME}]: " HOSTNAME
 HOSTNAME=${HOSTNAME:-$DEFAULT_HOSTNAME}
@@ -41,12 +45,13 @@ KEYMAP=${KEYMAP:-$DEFAULT_KEYMAP}
 
 echo
 echo "=== SUMMARY ==="
-echo "Disk:        $DISK"
-echo "Hostname:    $HOSTNAME"
-echo "Username:    $USERNAME"
-echo "Timezone:    $TIMEZONE"
-echo "Locale:      $LOCALE"
-echo "Keymap:      $KEYMAP"
+echo "Disk:         $DISK"
+echo "Encrypt root: $ENCRYPT"
+echo "Hostname:     $HOSTNAME"
+echo "Username:     $USERNAME"
+echo "Timezone:     $TIMEZONE"
+echo "Locale:       $LOCALE"
+echo "Keymap:       $KEYMAP"
 echo "================"
 read -rp "Type YES to continue: " CONFIRM
 [[ "$CONFIRM" == "YES" ]] || { echo "Aborted."; exit 1; }
@@ -65,28 +70,33 @@ parted -s "$DISK" set 1 esp on
 parted -s "$DISK" mkpart primary ext4 301MiB 100%
 
 EFI_PART="${DISK}1"
-CRYPT_PART="${DISK}2"
+ROOT_PART="${DISK}2"
 [[ "$DISK" == *"nvme"* ]] && {
   EFI_PART="${DISK}p1"
-  CRYPT_PART="${DISK}p2"
+  ROOT_PART="${DISK}p2"
 }
 
 mkfs.fat -F32 -n EFI "$EFI_PART"
 
 # ================================================================
-#  Encryption Setup
+#  Optional Encryption Setup
 # ================================================================
-echo "[2/10] Setting up LUKS2 encryption on $CRYPT_PART..."
-cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 \
-  --key-size 512 --hash sha512 --iter-time 5000 "$CRYPT_PART"
-
-cryptsetup open "$CRYPT_PART" cryptroot
+if [[ "$ENCRYPT" =~ ^[Yy]$ ]]; then
+  echo "[2/10] Setting up LUKS2 encryption on $ROOT_PART..."
+  cryptsetup luksFormat --type luks2 --cipher aes-xts-plain64 \
+    --key-size 512 --hash sha512 --iter-time 5000 "$ROOT_PART"
+  cryptsetup open "$ROOT_PART" cryptroot
+  ROOT_MAPPER="/dev/mapper/cryptroot"
+else
+  echo "[2/10] Skipping encryption for root..."
+  ROOT_MAPPER="$ROOT_PART"
+fi
 
 # ================================================================
 #  Filesystems and Mounting
 # ================================================================
 echo "[3/10] Formatting and mounting..."
-mkfs.ext4 -L ROOT /dev/mapper/cryptroot
+mkfs.ext4 -L ROOT "$ROOT_MAPPER"
 mount -L ROOT /mnt
 mkdir -p /mnt/boot
 mount "$EFI_PART" /mnt/boot
@@ -119,21 +129,28 @@ echo "LANG=$LOCALE" > /etc/locale.conf
 echo "[7/10] Setting hostname..."
 echo "$HOSTNAME" > /etc/hostname
 
-# Setup mkinitcpio hooks for encryption
-echo "[8/10] Configuring initramfs..."
-sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+# Setup mkinitcpio hooks for encryption if needed
+if [[ "$ENCRYPT" =~ ^[Yy]$ ]]; then
+  echo "[8/10] Configuring initramfs for encryption..."
+  sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block encrypt filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+else
+  echo "[8/10] Configuring standard initramfs..."
+  sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/' /etc/mkinitcpio.conf
+fi
 mkinitcpio -P
-
-# Detect LUKS UUID
-CRYPTUUID=\$(blkid -s UUID -o value $CRYPT_PART)
 
 # Configure GRUB
 echo "[9/10] Installing and configuring GRUB bootloader..."
 sed -i '/^GRUB_ENABLE_CRYPTODISK=/d' /etc/default/grub
-echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
-sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=\$CRYPTUUID:cryptroot root=/dev/mapper/cryptroot\"|" /etc/default/grub
 
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux --modules="luks cryptodisk" --recheck
+if [[ "$ENCRYPT" =~ ^[Yy]$ ]]; then
+  echo 'GRUB_ENABLE_CRYPTODISK=y' >> /etc/default/grub
+  CRYPTUUID=\$(blkid -s UUID -o value $ROOT_PART)
+  sed -i "s|^GRUB_CMDLINE_LINUX=.*|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=\$CRYPTUUID:cryptroot root=/dev/mapper/cryptroot\"|" /etc/default/grub
+fi
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=ArchLinux \
+  $([[ "$ENCRYPT" =~ ^[Yy]$ ]] && echo '--modules="luks cryptodisk"') --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Networking
@@ -168,10 +185,12 @@ arch-chroot /mnt /root/chroot_config.sh
 echo "[*] Cleaning up..."
 rm /mnt/root/chroot_config.sh
 umount -R /mnt
-cryptsetup close cryptroot
+if [[ "$ENCRYPT" =~ ^[Yy]$ ]]; then
+  cryptsetup close cryptroot
+fi
 
 echo "=== Installation Complete ==="
-echo "You can now reboot into your encrypted Arch Linux system!"
+echo "You can now reboot into your Arch Linux system!"
 echo "Rebooting in 10 seconds..."
 sleep 10
 reboot
